1fc8348a099b06a9c8cba062e10c8ab630f92a56
Unknown <maghe@mind.cc>
2017-11-17 16:08:02 +0100

    Code refactored
     - Removed unused code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <algorithm>
 #include "tempsensor_DCPS.hpp"
-#include "implementation.h"
 #include "DDSManager.h"
 #include "Items.h"
 #include <thread>
@@ -23,29 +22,8 @@ public:
 
 int main() {
 
-    /*
-    dds::domain::DomainParticipant dp(2); //Change the value also in ospl.xml
-
-    dds::core::policy::Durability durability = dds::core::policy::Durability::Persistent();
-    //dds::core::policy::Durability durability = dds::core::policy::Durability::Volatile();
-    dds::core::policy::Reliability reliability = dds::core::policy::Reliability::Reliable();
-    dds::core::policy::History history = dds::core::policy::History::KeepLast(1);
-    dds::topic::qos::TopicQos topicQos =
-            dp.default_topic_qos() << durability << reliability << history;
-
-    /*dds::topic::Topic<Mind::PompaCalore> topic(dp, "PompaCalore", topicQos);
-    std::string partitionName = "PompaCalore";
-    DDSPublisher<dds::topic::Topic<Mind::PompaCalore>> ddsPublisher (partitionName);
-    ddsPublisher.write(topic);* /
-
-
-    dds::topic::Topic<TempSensorType> topic(dp, "TempSensorTopic", topicQos);
-     */
-
-
-
-    std::string partitionName = "prova";
-    DDSPublisher<TempSensorType> ddsPublisher (partitionName);
+    std::string partitionName = "DemoTempSensor";
+    DDSPublisher<TempSensorType> ddsPublisher(partitionName);
 
     TemperatureScale scale = TemperatureScale::CELSIUS;
     TempSensorType ts;
@@ -61,26 +39,19 @@ int main() {
     std::vector<TempSensorType> samples;
     reader.readAll(samples);
 
-    for(int i =0; i<samples.size();i++){
+    for (int i = 0; i < samples.size(); i++) {
 
-        std::cout<< "ID " << samples[i].id() << std::endl;
-        std::cout<< "Hum " << samples[i].hum() << std::endl;
-        std::cout<< "temp "<<samples[i].temp() << std::endl;
-       // std::cout<< "scale  "<<samples[i].scale() << std::endl;
+        std::cout << "ID " << samples[i].id() << std::endl;
+        std::cout << "Hum " << samples[i].hum() << std::endl;
+        std::cout << "temp " << samples[i].temp() << std::endl;
     }
 
-
-
-/*
+    /*
+    Example: read exists
     TempSensorType findSample;
     bool b = reader.exists(2);
-*/
+    */
 
-    //int i = examples::dcps::tempsensor::isocpp::publisher(dp, topic, partitionName);
-    //int k = examples::dcps::tempsensor::isocpp::subscriber(dp, topic, partitionName);
-    //int j = examples::dcps::tempsensor::isocpp::publisher(dp, topic, partitionName);
-
-
-        return 0;
+    return 0;
 }
 
