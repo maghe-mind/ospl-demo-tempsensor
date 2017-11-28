bf81ba2320a682acc6de61655dc2550f0969895b
Unknown <maghe@mind.cc>
2017-11-28 18:38:24 +0100

    Updated SensiboManager

--- sensiboManager/SensiboAcState.cpp
+++ sensiboManager/SensiboAcState.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "SensiboAcState.h"
+#include <iostream>
+
+
+SensiboAcState::SensiboAcState() {
+}
+
+
+SensiboAcState::SensiboAcState(std::string id, bool on, std::string fanLevel, std::string temperatureUnit,
+                               int targetTemperature, std::string mode, std::string swing) {
+
+    this->id = id;
+    this->on = on;
+    this->fanLevel = fanLevel;
+    this->temperatureUnit = temperatureUnit;
+    this->targetTemperature = targetTemperature;
+    this->mode = mode;
+    this->swing = swing;
+}
--- sensiboManager/SensiboAcState.h
+++ sensiboManager/SensiboAcState.h
@@ -2,13 +2,27 @@
 // Created by maghe on 28/11/17.
 //
 
-#ifndef TEMPSENSOR_SENSIBOACSTATE_H
-#define TEMPSENSOR_SENSIBOACSTATE_H
+#ifndef SENSIBO_SENSIBOACSTATE_H
+#define SENSIBO_SENSIBOACSTATE_H
 
+#include <iostream>
 
 class SensiboAcState {
 
+public:
+    SensiboAcState();// TODO: is it the correct way?
+    SensiboAcState(std::string id, bool on, std::string fanLevel, std::string temperatureUnit, int targetTemperature,
+                   std::string mode, std::string swing);
+
+private:
+    std::string id;
+    bool on;
+    std::string fanLevel;
+    std::string temperatureUnit;
+    int targetTemperature;
+    std::string mode;
+    std::string swing;
 };
 
 
-#endif //TEMPSENSOR_SENSIBOACSTATE_H
+#endif //SENSIBO_SENSIBOACSTATE_H
