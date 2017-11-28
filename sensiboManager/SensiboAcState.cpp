a029d89f4cee0d9d4b258e1e53078c9fcd40e9e7
Unknown <maghe@mind.cc>
2017-11-28 18:37:36 +0100

    Added sensibo AcState

---
+++ sensiboManager/SensiboAcState.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maghe on 28/11/17.
+//
+
+#include "SensiboAcState.h"
---
+++ sensiboManager/SensiboAcState.h
@@ -0,0 +1,14 @@
+//
+// Created by maghe on 28/11/17.
+//
+
+#ifndef TEMPSENSOR_SENSIBOACSTATE_H
+#define TEMPSENSOR_SENSIBOACSTATE_H
+
+
+class SensiboAcState {
+
+};
+
+
+#endif //TEMPSENSOR_SENSIBOACSTATE_H
